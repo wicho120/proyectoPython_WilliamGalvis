0574870c9179c03586da2d825b6051567c296cfe
Wicho120 <wgalvis1228@gmail.com>
2024-02-19 16:35:17 -0500

    first commit

--- modulos_Python/menus.py
+++ modulos_Python/menus.py
@@ -26,13 +26,13 @@ def menuCoordinador():
                 if opc == 4:
                     funcionesMatricula.crearGruposMatriculas()
                 if opc == 5:
-                    
+                    menuReportes()
                 if opc == 6:
                     break
         except ValueError:
             print()
 
-def menuCoordinador():
+def menuReportes():
 
     menu=["Ver Campers Incritos","Ver Campers Aprobados","Ver Trainers Trabajando","Salir"]
     while(True):
@@ -61,7 +61,7 @@ def menuCoordinador():
 
 def menuCamper():
 
-    menu=["Registrarse","Ver historico de notas","Salir"]
+    menu=["Registrarse","Salir"]
     while(True):
         os.system('cls')
         print(""""
@@ -77,13 +77,33 @@ def menuCamper():
                 print(">>")
                 if opc == 1:
                     funcionesCamper.crearCamper()
-                if opc == 3:
-                    print("opcion 2")
-                if opc == 3:
+                if opc == 2:
                     break
         except ValueError:
             print("Usuario no *** no recibime numero")
 
+def menuCamper():
+
+    menu=["Calificar Modulo Camper","Salir"]
+    while(True):
+        os.system('cls')
+        print(""""
+        ..............................
+                .:CAMPUSLANDS:.
+        ............CAMPER............
+        
+            """)
+        print("".join([f"{i+1}. {val}\n" for i,val in enumerate(menu)]))
+        try:
+            opc=int(input())
+            if(opc<=len(menu) and opc>0):
+                print(">>")
+                if opc == 1:
+                    funcionesCamper.crearCamper()
+                if opc == 2:
+                    break
+        except ValueError:
+            print("Usuario no *** no recibime numero")
 
 
 def menuPrincipal():
